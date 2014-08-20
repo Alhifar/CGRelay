1a813d0ddcee6952e7a2786e83ff7d0e8a72182c
Alhifar <alhifar@gmail.com>
2014-08-20 14:24:14 -0400

    Handle bold and italic text from forum

--- modules/relay.py
+++ modules/relay.py
@@ -40,6 +40,8 @@ def getFromRelay(bot):
 			message = messageMatch.group(3)
 			message = re.sub( r'<img src="\./images/smilies.+?alt="([^"]+?)".+?/>', r'\1', message ) #Replace smilies from forum
 			message = re.sub( r'<a.*?href=\"(.+?)".*?>.*?</a>', r'\1', message ) #Replace links with just url
+			message = re.sub( r'<span style="font-style: italic">(.+?)</span>', '\x1D\\1\x1D', message ) #Italics
+			message = re.sub( r'<span style="font-weight: bold">(.+?)</span>', '\x02\\1\x02', message ) #Bold
 			message = re.sub( r'<.*?>', '', message ) #Remove all other tags
 			openBracket = "{"
 			closeBracket = "}"
