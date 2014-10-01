2b5c8dfd5257ecc0d6790f0c8f6b97d24b34b696
Alhifar <alhifar@gmail.com>
2014-10-01 10:50:40 -0400

    Get SID from login page before logging in

--- pykol/RelayBot.py
+++ pykol/RelayBot.py
@@ -1,6 +1,7 @@
 from time import sleep
 from kol.Session import Session
 from kol.manager.ChatManager import ChatManager
+from bs4 import BeautifulSoup
 import requests
 import re
 
@@ -9,16 +10,21 @@ class RelayBot(object):
 		self.session = Session()
 		self.password = 'thepassword'
 		self.forumPassword = 'theotherpassword'
-		self.session.login( 'Alhifar', self.password )
+		self.session.login( 'CGRelay', self.password )
 		self.chatManager = ChatManager(self.session)
 		self.SID = ''
-	def forumLogin():
-		params = {'mode': 'login', 'username': 'CGBot', 'password': self.forumPassword}
-		r = requests.post('http://www.crimbogrotto.com/ucp.php', params=params)
-		m = re.match(r'\?sid=([^"]+)"', r.text)
-		self.SID = m.group(0)
-	def runBot():
-		forumLogin()
+	
+	def forumLogin(self):
+		loginPageResponse = requests.post('http://www.crimbogrotto.com/ucp.php', params={'mode': 'login'})
+		loginPageSoup = BeautifulSoup(loginPageResponse.text)
+		self.SID = loginPageSoup.find('input', {'name': 'sid'})['value']
+		
+		params = {'username': 'CGBot', 'password': self.forumPassword, 'sid': self.SID, 'redirect': './ucp.php?mode=login', 'login': 'Login'}
+		r = requests.post('http://www.crimbogrotto.com/ucp.php?mode=login', params=params)
+		print(r.url)
+	
+	def runBot(self):
+		self.forumLogin()
 		while True:
 			chatMessages = self.chatManager.getNewChatMessages()
 			for message in chatMessages:
