f905f7daa4ac6786244d23351ec3b3545782e020
Alhifar <alhifar@gmail.com>
2014-12-09 20:11:33 -0500

    Refix linking and regexes.

--- modules/relay.py
+++ modules/relay.py
@@ -1,11 +1,11 @@
 from willie import module
 from willie import web
-# from willie import tools
+from willie import tools
 import willie.irc as irc
 import re
 import HTMLParser
 
-#logger = tools.OutputRedirect("/home/crimbogrotto/.willie/logs/relaylog.log")
+logger = tools.OutputRedirect("/home/crimbogrotto/.willie/logs/relaylog.log")
 
 #Patterns for relay()
 actionPattern = re.compile(r'^\x01ACTION')
@@ -31,10 +31,10 @@ whoPattern = re.compile(
 def relay(bot, trigger):
     #global logger
     sid = bot.getForumSID()
-    message = actionPattern.sub('', trigger.group(0))  #Remove ACTION from /me messages
-    message = colorPattern.sub('', message)  # Remove color codes
-    message = nonAsciiPattern.sub('', message)  #remove non-ascii characters 'cause forum chat and kol chat are dumb
-    message = hiddenPattern.sub('', message)  #messages starting with "irc:" are hidden from kol chat
+    message = re.sub(actionPattern, '', trigger.group(0))  #Remove ACTION from /me messages
+    message = re.sub(colorPattern, '', message)  # Remove color codes
+    message = re.sub(nonAsciiPattern, '', message)  #remove non-ascii characters 'cause forum chat and kol chat are dumb
+    message = re.sub(hiddenPattern, '', message)  #messages starting with "irc:" are hidden from kol chat
     if message == '':
         return
     toPost = web.quote(u'[b]{0}:[/b] {1}'.format(trigger.nick, message))
@@ -61,35 +61,40 @@ def postIRCWho(bot, trigger=None):
 
 @module.interval(3)
 def getFromRelay(bot):
-    #global logger
+    global logger
     if not "#crimbogrotto" in bot.channels:
         return
     sid = bot.getForumSID()
     if not 'lastMChatID' in bot.memory.keys():
         text = web.post('http://www.crimbogrotto.com/mchat.php', 'mode=read&room_id=0&sid={0}'.format(sid))
-        messageList = mchatPattern.findall(text)
+        messageList = re.findall(mchatPattern, text)
         bot.memory['lastMChatID'] = int(messageList[-1][0])
 
     params = 'mode=read&room_id=0&message_last_id={0!s}&sid={1}'.format(bot.memory['lastMChatID'], sid)
     text = web.post('http://www.crimbogrotto.com/mchat.php', params)
-    messageIter = mchatPattern.finditer(text)
+    messageIter = re.finditer(mchatPattern, text)
     parser = HTMLParser.HTMLParser()
     for messageMatch in messageIter:
         if messageMatch.group(2) != 'CGIRC':
             sender = messageMatch.group(2)
             message = messageMatch.group(3)
-            message = smiliesPattern.sub(r'\1', message)  #Replace smilies from forum
-            linkMatch = linkPattern.match(message)
+            message = re.sub(smiliesPattern, r'\1', message)  #Replace smilies from forum
+            linkMatch = re.search(linkPattern, message)
             if linkMatch:
-                if linkMatch.group(1) != linkMatch.group(2):
-                    message = linkPattern.sub(r'\2 (\1)', message)  #Replace links with url and text
+                if linkMatch.group(1) != linkMatch.group(2) and linkMatch.group(2) != '[link]':
+                    message = re.sub(linkPattern, r'\1 (\2)', message)  #Replace links with url and text
                 else:
-                    message = linkPattern.sub(r'\1', message)
-            message = tagPattern.sub('', message)  #Remove all other tags
+                    print(message)
+                    print('\n')
+                    print(linkMatch.group(1))
+                    print('\n')
+                    print(linkMatch.group(0))
+                    message = re.sub(linkPattern, r'\1', message)
+            message = re.sub(tagPattern, '', message)  #Remove all other tags
             openBracket = '{'
             closeBracket = '}'
             if sender == 'CGBot':
-                nameMatch = CGBotMessagePattern.match(message)
+                nameMatch = re.match(CGBotMessagePattern, message)
                 sender = nameMatch.group(1)
                 message = nameMatch.group(2)
                 openBracket = '['
@@ -113,7 +118,7 @@ def denisKick(bot, trigger):
 def who(bot, trigger):
     sid = bot.getForumSID()
     whoText = web.post('http://www.crimbogrotto.com/mchat.php', 'mode=read&room_id=6&sid={0}'.format(sid))
-    messageList = whoPattern.findall(whoText)
+    messageList = re.findall(whoPattern, whoText)
     bot.msg(trigger.nick, messageList[-1][2].decode('utf-8'), relay=False)
 
 
